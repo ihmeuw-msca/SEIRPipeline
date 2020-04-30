2f821257a107305f827dc58202d6dd83213c2c8d
Marlena Bannick <mnorwood@uw.edu>
2020-04-30 10:14:13 -0700

    A rebase

--- src/seiir_model/utils.py
+++ src/seiir_model/utils.py
@@ -1,2 +1,21 @@
+from slime.core.data import MRData
 
 SEIIR_COMPARTMENTS = ['S', 'E', 'I1', '12', 'R']
+
+COL_T = 'days'
+COL_BETA = 'beta'
+COL_GROUP = 'loc_id'
+
+
+def convert_inputs_for_beta_model(data_cov, df_beta, covmodel_set):
+    df_cov, col_t_cov, col_group_cov = data_cov
+    df = df_beta.merge(
+        df_cov, 
+        left_on=[COL_T, COL_GROUP], 
+        right_on=[col_t_cov, col_group_cov],
+    ).copy()
+    df.sort_values(inplace=True, by=[COL_GROUP, COL_T])
+    cov_names = [covmodel.col_cov for covmodel in covmodel_set.cov_models]
+    mrdata = MRData(df, col_group=COL_GROUP, col_obs=COL_BETA, col_covs=cov_names)
+
+    return mrdata
