b780d9ee5acfa862d7862713ae4f5b2d8bf30292
jizezhang <jizez@uw.edu>
2020-05-02 12:28:57 -0700

    added beta-sequential

--- src/seiir_model/regression_model/beta_fit.py
+++ src/seiir_model/regression_model/beta_fit.py
@@ -12,22 +12,16 @@ class BetaRegressor:
         self.covmodel_set = covmodel_set
         self.col_covs = [covmodel.col_cov for covmodel in covmodel_set.cov_models]
 
-    def set_fe(self, mr_data, std):
-        covmodel_set_fe = copy.deepcopy(self.covmodel_set)
-        for covmodel in covmodel_set_fe.cov_models:
+    def fit_no_random(self, mr_data):
+        self.covmodel_set_fixed = copy.deepcopy(self.covmodel_set)
+        for covmodel in self.covmodel_set_fixed.cov_models:
             covmodel.use_re = False 
 
-        mr_model_fe = MRModel(mr_data, covmodel_set_fe)
-        mr_model_fe.fit_model()
-        fe = list(mr_model_fe.result.values())[0]
-        for v, covmodel in zip(fe, self.covmodel_set.cov_models):
-            covmodel.gprior = np.array([v, std])
+        self.mr_model_fixed = MRModel(mr_data, self.covmodel_set_fixed)
+        self.mr_model_fixed.fit_model()
+        self.cov_coef_fixed = list(self.mr_model_fixed.result.values())[0]
 
-    def fit(self, mr_data, two_stage=False, std=None):
-        if two_stage:
-            assert std is not None
-            self.set_fe(mr_data, std)
-        
+    def fit(self, mr_data):        
         self.mr_model = MRModel(mr_data, self.covmodel_set) 
         self.mr_model.fit_model()
         self.cov_coef = self.mr_model.result
@@ -55,6 +49,36 @@ class BetaRegressor:
             return np.sum([self.cov_coef[group][i] * cov[:, i] for i in range(cov.shape[1])], axis=0)
         else:
             raise RuntimeError('Group Not Found.')
+
+
+class BetaRegressorSequential:
+
+    def __init__(self, ordered_covmodel_sets, std=1e-7):
+        self.ordered_covmodel_sets = copy.deepcopy(ordered_covmodel_sets)
+        self.std = std
+        self.col_covs = []
+        for covmodel_set in self.ordered_covmodel_sets:
+            self.col_covs.extend([covmodel.col_cov for covmodel in covmodel_set.cov_models])
+    
+    def fit(self, mr_data):
+        covmodels = []
+        while len(self.ordered_covmodel_sets) > 0:
+            covmodel_set = CovModelSet(covmodels + self.ordered_covmodel_sets.pop(0).cov_models)
+            regressor = BetaRegressor(covmodel_set)
+            regressor.fit_no_random(mr_data)
+            for covmodel, coef in zip(covmodel_set.cov_models, regressor.cov_coef_fixed):
+                covmodel.gprior = [coef, self.std]
+            covmodels = covmodel_set.cov_models
+
+        self.regressor = BetaRegressor(CovModelSet(covmodels))
+        self.regressor.fit(mr_data)
+        self.cov_coef = self.regressor.cov_coef
+
+    def save_coef(self, path):
+        self.regressor.save_coef(path)
+
+    def load_coef(self, df=None, path=None):
+        self.regressor.load_coef(df=df, path=path)
 
 
 def predict(regressor, df_cov, col_t, col_group, col_beta='beta_pred'):
