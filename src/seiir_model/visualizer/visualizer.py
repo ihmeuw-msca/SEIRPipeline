13b55a98edf44dc035ed2f641689cd0fb4b14277
zhengp0 <zhengp@uw.edu>
2020-05-11 10:11:31 -0700

    tuning the plotting.

--- src/seiir_model/visualizer/visualizer.py
+++ src/seiir_model/visualizer/visualizer.py
@@ -337,7 +337,7 @@ class PlotBetaCoef:
 
     def plot_coef(self):
         for cov in self.covs:
-            plt.figure(figsize=(8, self.coef_data[cov][1].shape[0]//4))
+            plt.figure(figsize=(8, self.coef_data[cov][1].shape[1]//4))
             plt.boxplot(self.coef_data[cov][1], vert=False, showfliers=False,
                         boxprops=dict(linewidth=0.5),
                         whiskerprops=dict(linewidth=0.5))
@@ -419,7 +419,7 @@ class PlotBetaScaling:
         self.path_to_location_metadata = self.directories.get_location_metadata_file(
             self.settings.location_set_version_id)
         self.path_to_beta_scaling = self.directories.forecast_beta_scaling_dir
-        self.path_to_savefig = self.directories.regression_diagnostic_dir
+        self.path_to_savefig = self.directories.forecast_diagnostic_dir
 
         # load location metadata
         self.location_metadata = pd.read_csv(self.path_to_location_metadata)
@@ -437,23 +437,27 @@ class PlotBetaScaling:
         ])
 
         # load data
-        self.scales_data = np.vstack([
+        self.scales_data = np.hstack([
             pd.read_csv(self.directories.location_beta_scaling_file(loc_id))[
                 'beta_scales'
-            ].values.ravel()
+            ].values[:, None]
             for loc_id in self.loc_ids
         ])
 
     def plot_scales(self):
+        scales_mean = self.scales_data.mean(axis=0)
+        sort_id = np.argsort(scales_mean)
         plt.figure(figsize=(8, len(self.locs)//4))
-        plt.boxplot(self.scales_data, vert=False, showfliers=False,
+        plt.boxplot(self.scales_data[:, sort_id], vert=False, showfliers=False,
                     boxprops=dict(linewidth=0.5),
                     whiskerprops=dict(linewidth=0.5))
         plt.yticks(ticks=np.arange(len(self.locs)) + 1,
-                   labels=self.locs)
+                   labels=self.locs[sort_id])
         plt.grid(b=True)
         plt.box(on=None)
         plt.title('beta scalings')
+        plt.vlines(1.0, ymin=1, ymax=len(self.locs),
+                   linewidth=1.0, linestyle='--', color='#8B0000')
         plt.savefig(self.path_to_savefig/f'beta_scalings_boxplot.pdf',
                     bbox_inches='tight')
 
